48dd03f4ef69e5787a8c968cc1f4fcd89b98d454
erlonpr <erlonpr@gmail.com>
2024-10-18 19:01:57 +0000

    feat(routes): add error handling in get method for book routes
    - added a try-catch block in the get method to simulate error handling by throwing a test error
    
    - updated the catch block to return a 500 status code and send the error message as the response

--- routes/book.js
+++ routes/book.js
@@ -3,7 +3,31 @@ const { Router } = require('express'); // import router from express
 const router = Router() // instance router
 
 router.get('/', (req, res) => { // define root route with GET method
-  res.send('GET method') // send response
+  try {
+
+    throw new Error('Test error') // throw error simulation
+
+    /*
+      Due to the error being thrown 
+        throw new Error('Test error')
+      the line 
+        res.send('GET method')
+      will never be executed, because the error stops execution before that line
+    */
+
+    res.send('GET method') // send response, case success (status code 200 - ok)    
+  
+  } catch (error) {
+    
+    /*
+      The catch receives the error object as an argument, usually named error, 
+      but can be named differently, such as: err, e
+    */
+
+    res.status(500) // send response, case error (status code 500 - internal server error)    
+    res.send(error.message)  
+    
+  }  
 })
 
 router.post('/', (req, res) => { // define root route with POST method
